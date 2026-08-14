ec6aa0c8538a05b56f91e66561e67a6c4b0e6b36
kllzrose <benjaminariel727@gmail.com>
2026-08-13 21:48:34 -0300

    Add files via upload

---
+++ 2ER EJERCICIO/SolucionCapas.Presentacion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Ejercicio2.Negocio;
+ 
+namespace Ejercicio2.Presentacion
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Ingrese el número de Legajo (ej: 10042): ");
+            if (int.TryParse(Console.ReadLine(), out int legajo))
+            {
+                AlumnoNegocio negocio = new AlumnoNegocio();
+                Alumno alumno = negocio.ObtenerAlumno(legajo);
+
+                if (alumno != null)
+                    Console.WriteLine($"Encontrado -> Alumno: {alumno.Nombre}, Condición: {alumno.Condicion}");
+                else
+                    Console.WriteLine("No existe el alumno o el legajo debe ser mayor a cero.");
+            }
+            else
+            {
+                Console.WriteLine("Por favor, ingrese un número válido.");
+            }
+        }
+    }
+}
